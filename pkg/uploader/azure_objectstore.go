ab4a64e1955719aae1c4fc217b1ac9d84c2ad643
Scott Seago <sseago@redhat.com>
2026-07-21 16:11:29 -0400

    removed unnecessary block

--- pkg/uploader/azure_objectstore.go
+++ pkg/uploader/azure_objectstore.go
@@ -101,10 +101,6 @@ func (a *AzureObjectStore) Init(configMap map[string]string) error {
 		configMap["storageAccount"] = os.Getenv("AZURE_STORAGE_ACCOUNT")
 	}
 
-	// Tell Velero which key in the credentials file contains the access key
-	if configMap["storageAccountKeyEnvVar"] == "" {
-		configMap["storageAccountKeyEnvVar"] = "AZURE_STORAGE_ACCOUNT_ACCESS_KEY"
-	}
 
 	logger := logrus.New()
 
